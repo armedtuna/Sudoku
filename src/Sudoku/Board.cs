828b5ff2a0c6f95761776449a9b09aaefb35205c
Anton Tolken <armedtuna@gmail.com>
2020-01-30 19:38:43 +1300

    add border drawing

--- src/Sudoku/Board.cs
+++ src/Sudoku/Board.cs
@@ -16,13 +16,23 @@ namespace Sudoku
         public string ToActualString()
         {
             var sb = new StringBuilder();
+            sb.Append("+---+---+---+" + Environment.NewLine);
             for (var rowIndex = Cells.GetLowerBound(0); rowIndex <= Cells.GetUpperBound(0); rowIndex++)
             {
+                sb.Append('|');
                 for (var columnIndex = Cells.GetLowerBound(1); columnIndex <= Cells.GetUpperBound(1); columnIndex++)
                 {
                     sb.Append(Cells[rowIndex, columnIndex].ToActualString());
+                    if (columnIndex % 3 == 2)
+                    {
+                        sb.Append('|');
+                    }
                 }
                 sb.Append(Environment.NewLine);
+                if (rowIndex % 3 == 2)
+                {
+                    sb.Append("+---+---+---+" + Environment.NewLine);
+                }
             }
 
             return sb.ToString();
